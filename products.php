ff0049fcdfabdb346dcde6c71522b0b9ab1b4c7f
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-03-12 18:36:07 +0100

    Reglage du bouton menu sur le mode mobile

---
+++ products.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<!--
+	Landed by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+<head>
+    <title>Posthings</title>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery.scrolly.min.js"></script>
+    <script src="js/jquery.dropotron.min.js"></script>
+    <script src="js/jquery.scrollex.min.js"></script>
+    <script src="js/skel.min.js"></script>
+    <script src="js/skel-layers.min.js"></script>
+    <script src="js/init.js"></script>
+    <noscript>
+        <link rel="stylesheet" href="css/skel.css" />
+        <link rel="stylesheet" href="css/style.css" />
+        <link rel="stylesheet" href="css/style-xlarge.css" />
+    </noscript>
+    <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+    <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+</head>
+<body class="landing">
+
+<!-- Header -->
+<header id="header">
+    <h1 id="logo"><a href="index.html">Posthings</a></h1>
+    <nav id="nav">
+        <ul>
+            <li><a href="index.html">Accueil</a></li>
+            <li>
+                <a href="">Produits</a>
+                <ul>
+                    <li><a href="left-sidebar.html">Achat/Vente</a></li>
+                    <li><a href="right-sidebar.html">Cours Particuliers</a></li>
+                    <li><a href="no-sidebar.html">Evenements</a></li>
+
+                </ul>
+            </li>
+            <li><a href="elements.html">Nous ?</a></li>
+            <li><a href="#" class="button special">Inscrivez-Vous</a></li>
+        </ul>
+    </nav>
+</header>
+
+
+
+<section id="two" class="wrapper style2 special fade">
+    <div class="container">
+        <header>
+            <h2>Que recherchez-vous ?</h2>
+        </header>
+        <form method="post" action="#" class="container 50%">
+            <div class="row uniform 50%">
+                <div class="8u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Votre Nom Ici" /></div>
+                    <div class="select-wrapper">
+                        <select name="category" id="category">
+                            <option value="">- Category -</option>
+                            <option value="1">Manufacturing</option>
+                            <option value="1">Shipping</option>
+                            <option value="1">Administration</option>
+                            <option value="1">Human Resources</option>
+                        </select>
+                    </div>
+
+
+                <div class="4u$ 12u$(xsmall)"><input type="submit" value="Go" class="fit special" /></div>
+            </div>
+        </form>
+    </div>
+</section>
+
+
+<section >
+    <div id="main" class="wrapper style1">
+        <div class="container">
+            <div class="box alt">
+                <div class="row 50% uniform">
+                    <div class="12u$"><span class="image fit"><img src="images/pic07.jpg" alt=""></span></div>
+                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
+                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
+                    <div class="4u$ 6u(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
+                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
+                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
+                    <div class="4u$ 6u$(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
+                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
+                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
+                    <div class="4u$ 6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
+                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
+                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
+                    <div class="4u$ 6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Footer -->
+<footer id="footer">
+    <ul class="icons">
+        <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+        <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+        <li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
+        <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+        <li><a href="https://github.com/AminMe/Projet-WebBD-AK-HE-ME-ZH" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+        <li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+    </ul>
+    <ul class="copyright">
+        <h1 id="logofooter"><a href="index.html">Posthings</a></h1>
+        <br/>
+        <li>&copy; Copyright Reserved <a>Posthings.com</a></li>
+    </ul>
+</footer>
+
+</body>
+</html>
