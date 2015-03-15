b4e4be799d5ca7479b08a2b86b70b33e264a4183
Amin Messaoudi <amin_messaoudi@yahoo.com>
2015-03-15 15:42:55 +0100

    maj je la page products.php

--- products.php
+++ products.php
@@ -6,7 +6,7 @@
 -->
 <html>
 <head>
-    <title>Posthings</title>
+    <title>Produits</title>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <meta name="description" content="" />
     <meta name="keywords" content="" />
@@ -18,19 +18,24 @@
     <script src="js/skel.min.js"></script>
     <script src="js/skel-layers.min.js"></script>
     <script src="js/init.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
     <noscript>
+
         <link rel="stylesheet" href="css/skel.css" />
         <link rel="stylesheet" href="css/style.css" />
         <link rel="stylesheet" href="css/style-xlarge.css" />
-
     </noscript>
+    <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="stylesheet" href="css/products.css" />
+
     <!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
     <!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
 </head>
-<body class="landing">
+<body>
 
 <!-- Header -->
-<header id="header">
+<header id="header" class="skel-layers-fixed">
     <h1 id="logo"><a href="index.html">Posthings</a></h1>
     <nav id="nav">
         <ul>
@@ -41,6 +46,15 @@
                     <li><a href="left-sidebar.html">Achat/Vente</a></li>
                     <li><a href="right-sidebar.html">Cours Particuliers</a></li>
                     <li><a href="no-sidebar.html">Evenements</a></li>
+                    <!--<li>
+                        <a href="">Submenu</a>
+                        <ul>
+                            <li><a href="#">Option 1</a></li>
+                            <li><a href="#">Option 2</a></li>
+                            <li><a href="#">Option 3</a></li>
+                            <li><a href="#">Option 4</a></li>
+                        </ul>
+                    </li>-->
                 </ul>
             </li>
             <li><a href="elements.html">Nous ?</a></li>
@@ -49,56 +63,43 @@
     </nav>
 </header>
 
-
-<section id="four" class="wrapper style2 special fade">
+<!-- Main -->
+<div id="main" class="wrapper style1">
     <div class="container">
-        <header>
-            <h2>Que recherchez-vous ?</h2>
-        </header>
-        <form method="post" action="#" class="container 50%">
-            <div class="row uniform 50%">
-                <div class="8u 12u$(xsmall)"><input type="text" name="search" id="search" placeholder="Tapez ce qui vous passe par la tête ICI ..." /></div>
-                    <div class="select-wrapper">
-                        <select name="category" id="category">
-                            <option value="">- Catégorie -</option>
-                            <option value="1">Ordinateur</option>
-                            <option value="1">Montre</option>
-                            <option value="1">Sac</option>
-                            <option value="1">Cours</option>
-                        </select>
-                    </div>
+        <header class="major">
 
 
-                <div class="4u$ 12u$(xsmall)"><input type="submit" value="Go" class="fit special" /></div>
-            </div>
-        </form>
-    </div>
-</section>
-
-
-<section>
-    <div id="main" class="wrapper style1">
-        <div class="container">
-            <div class="box alt">
-                <div class="row 50% uniform">
-                    <div class="12u$"><span class="image fit"><img src="images/pic07.jpg" alt=""></span></div>
-                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
-                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
-                    <div class="4u$ 6u(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
-                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
-                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
-                    <div class="4u$ 6u$(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
-                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
-                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
-                    <div class="4u$ 6u(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
-                    <div class="4u  6u$(xsmall)"><span class="image fit"><img src="images/pic02.jpg" alt=""></span></div>
-                    <div class="4u  6u(xsmall)"><span class="image fit"><img src="images/pic03.jpg" alt=""></span></div>
-                    <div class="4u$ 6u$(xsmall)"><span class="image fit"><img src="images/pic04.jpg" alt=""></span></div>
+                <div class="container search">
+                    <header>
+                        <h2>Que recherchez-vous ?</h2>
+                    </header>
+                    <form method="post" action="#" class="container 50%">
+                        <div class="row uniform 50%">
+                            <div class="8u 12u$(xsmall)"><input type="text" name="search" id="search" placeholder="Tapez ce qui vous passe par la tête ICI ..." /></div>
+                            <div class="select-wrapper">
+                                <select name="category" id="category">
+                                    <option value="">- Catégorie -</option>
+                                    <option value="1">Ordinateur</option>
+                                    <option value="1">Montre</option>
+                                    <option value="1">Sac</option>
+                                    <option value="1">Cours</option>
+                                </select>
+                            </div>
+                            <div class="4u$ 12u$(xsmall)"><input type="submit" value="Go" class="fit special" /></div>
+                        </div>
+                    </form>
                 </div>
-            </div>
-        </div>
+
+        </header>
+
+
+
+        <?php
+            @include ('list-products.php');
+        ?>
+
     </div>
-</section>
+</div>
 
 <!-- Footer -->
 <footer id="footer">
@@ -107,13 +108,11 @@
         <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
         <li><a href="#" class="icon alt fa-linkedin"><span class="label">LinkedIn</span></a></li>
         <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
-        <li><a href="https://github.com/AminMe/Projet-WebBD-AK-HE-ME-ZH" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+        <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
         <li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
     </ul>
     <ul class="copyright">
-        <h1 id="logofooter"><a href="index.html">Posthings</a></h1>
-        <br/>
-        <li>&copy; Copyright Reserved <a>Posthings.com</a></li>
+        <li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
     </ul>
 </footer>
 
